847beccedaa3cb9d217dc793714f3f7c45fa8bdf
Amimul Ehsan <1805056@ugrad.cse.buet.ac.bd>
2021-10-18 22:02:50 +0600

    Fine tuned models

--- src/main/java/com/rahi/moviecatelogservice/controller/MovieCatalogResource.java
+++ src/main/java/com/rahi/moviecatelogservice/controller/MovieCatalogResource.java
@@ -3,14 +3,10 @@ package com.rahi.moviecatelogservice.controller;
 import com.rahi.moviecatelogservice.model.CatalogItem;
 import com.rahi.moviecatelogservice.model.Movie;
 import com.rahi.moviecatelogservice.model.UserRating;
-import org.springframework.cloud.client.ServiceInstance;
-import org.springframework.cloud.client.discovery.DiscoveryClient;
-import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.client.RestTemplate;
-import org.springframework.web.reactive.function.client.WebClient;
 
 import java.util.List;
 import java.util.stream.Collectors;
@@ -26,47 +22,22 @@ import java.util.stream.Collectors;
 public class MovieCatalogResource {
 
     private final RestTemplate restTemplate;
-    private final WebClient.Builder webClientBuilder;
-    private final DiscoveryClient discoveryClient;
 
-    public MovieCatalogResource( RestTemplate restTemplate, WebClient.Builder webClientBuilder,
-                                 DiscoveryClient discoveryClient ) {
+    public MovieCatalogResource( RestTemplate restTemplate ) {
         this.restTemplate = restTemplate;
-        this.webClientBuilder = webClientBuilder;
-        this.discoveryClient = discoveryClient;
     }
 
-    @GetMapping("/getInstances")
-    public List<ServiceInstance> getServiceInstances() {
-        return discoveryClient.getInstances("ratings-data-service");
-    }
-
-    @GetMapping("/{userId}")
+    @RequestMapping("/{userId}")
     public List<CatalogItem> getCatalog( @PathVariable("userId") String userId ) {
 
-        //For each one call info service and get details
-        //Put them all together
+        UserRating userRating = restTemplate.getForObject("http://ratings-data-service/ratingsdata/user/" + userId, UserRating.class);
 
-        UserRating ratings = restTemplate.getForObject("http://ratings-data-service/ratingsData/user/" + userId,
-                UserRating.class);
-
-        assert ratings != null;
-        return ratings.getUserRatings().stream()
+        return userRating.getUserRatings().stream()
                 .map(rating -> {
-
-//                    Movie movie = webClientBuilder.build()
-//                            .get()
-//                            .uri("http://127.0.0.1:8082/movies/" + rating.getMovieId())
-//                            .retrieve()
-//                            .bodyToMono(Movie.class)
-//                            .block();
-
-
                     Movie movie = restTemplate.getForObject("http://movie-info-service/movies/" + rating.getMovieId(), Movie.class);
-
-                    assert movie != null;
-                    return new CatalogItem(movie.getName(), "Test", 4);
+                    return new CatalogItem(movie.getName(), movie.getDescription(), rating.getRating());
                 })
                 .collect(Collectors.toList());
+
     }
 }
